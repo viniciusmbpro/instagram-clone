0377f1285c6a16dd97304b591958f90b7f1a6021
Vinícius Marques Bezerra <viniciusmbpro@gmail.com>
2022-10-22 20:28:19 -0300

    Configuring templates for login and logout

--- instagram_clone/views/all.py
+++ instagram_clone/views/all.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-@login_required(login_url='users:login_page', redirect_field_name='next')
+@login_required(login_url='users:login', redirect_field_name='next')
 def home(request):
     return render(request, 'instagram_clone/pages/home.html', {
         'range': range(10),
