ac336bada4bac98037c85bfc67477f618fd0906c
Vinícius Marques Bezerra <viniciusmbpro@gmail.com>
2022-10-22 17:40:53 -0300

    Adding url path to login_create

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ app_name = "users"
 
 urlpatterns = [
     path('login/', views.login_page, name='login'),
+    path('login/create/', views.login_create, name='login_create'),
     path('signup/', views.signup_page, name='signup'),
     path('signup/create/', views.signup_create, name='signup_create'),
 ]
