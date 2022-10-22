e1abdd685b275566ff0cf3ed90e7daca48ebd862
Vinícius Marques Bezerra <viniciusmbpro@gmail.com>
2022-10-22 17:34:58 -0300

    Created login_create view and security details

--- users/views/all.py
+++ users/views/all.py
@@ -1,13 +1,41 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import RegisterForm
+from users.forms import LoginForm, RegisterForm
 
 
 def login_page(request):
-    return render(request, 'users/pages/login.html')
+    form = LoginForm()
+    return render(request, 'users/pages/login.html', {
+        'form': form,
+        'form_action': reverse('users:login_create'),
+    })
+
+
+def login_create(request):
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.sucess(request, 'Your are logged in.')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect(reverse('insta:home'))
 
 
 def signup_page(request):
