ae8db20120b55525ffb2030ada463ea67d4c63be
Vinícius Marques Bezerra <viniciusmbpro@gmail.com>
2022-10-26 16:04:30 -0300

    Created views for profile edit

--- instagram_clone/views/profile_view.py
+++ instagram_clone/views/profile_view.py
@@ -1,13 +1,16 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-from instagram_clone.models import Post, Like
-from instagram_clone.forms import ProfileForm
+from instagram_clone.forms import ProfileForm, UserForm
+from instagram_clone.models import Like, Post, Profile
 
 
 @login_required(login_url='users:login', redirect_field_name='next')
-def profile(request):
-    posts = Post.objects.filter(user=request.user)
+def profile(request, username):
+    posts = Post.objects.filter(user__username=username)
     num_posts = posts.count()
     dados: list[str] = []
     for post in posts:
@@ -18,15 +21,50 @@ def profile(request):
             post,
             num_likes,
         ))
+
+    profile = Profile.objects.filter(user__username=username).first()
     return render(request, 'instagram_clone/pages/profile.html', {
+        'profile': profile,
         'posts': dados,
         'num_posts': num_posts,
     })
 
 
 @login_required(login_url='users:login', redirect_field_name='next')
-def profile_edit(request):
-    form = ProfileForm()
-    return render(request, 'instagram_clone/pages/profile_edit.html', {
-        'form': form,
-    })
+def profile_edit(request, id):
+    profile = Profile.objects.filter(
+        user=request.user,
+    ).first()
+
+    if request.user.id != id:
+        raise Http404()
+
+    user_form = UserForm(
+        data=request.POST or None,
+        instance=request.user,
+    )
+    profile_form = ProfileForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+        instance=profile,
+    )
+
+    if user_form.is_valid() & profile_form.is_valid():
+        user = user_form.save(commit=False)
+        user.save()
+
+        profile = profile_form.save(commit=False)
+        profile.save()
+
+        messages.success(request, 'Save successfully!')
+        return redirect(reverse('insta:profile_edit', args=(id,)))
+
+    return render(
+        request,
+        'instagram_clone/pages/profile_edit.html',
+        context={
+            'user_form': user_form,
+            'profile_form': profile_form,
+            'form_action': reverse('insta:profile_edit', args=(id,)),
+        }
+    )
